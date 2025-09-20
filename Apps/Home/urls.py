40eb679f057f61a6077f79e6a26334471939851e
T460-CAVASQUEZ\RicardoV <kostasmanolas43@gmail.com>
2025-09-20 00:22:28 -0600

    funciona pagina inicio y acerca de

--- Apps/Home/urls.py
+++ Apps/Home/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from Apps.Home.views import HomeView
+from Apps.Home.views import HomeView, AcercaView
 
 app_name = 'Home'
 urlpatterns = [
     path('', HomeView.as_view(), name='homeapp'),
+    path('acerca/', AcercaView.as_view(), name='acerca_app'),
 ]
 
