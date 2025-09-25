8446b43f8cb19e821592720688ba70c7cd2afaba
CarmenG1610 <mlopezg114@miumg.edu.gt>
2025-09-24 22:06:18 -0700

    link de  menu  funciona pero falta modificar a como lo vamos a usar

--- Apps/Cliente/urls.py
+++ Apps/Cliente/urls.py
@@ -1,9 +1,9 @@
-
 from django.urls import path
-from .views import LoginView, CrearClienteView
+from .views import LoginView, CrearClienteView, MenuView
 
 app_name = 'Cliente'
 urlpatterns = [
 	path('login/', LoginView.as_view(), name='loginapp'),
 	path('registrar/', CrearClienteView.as_view(), name='registrarCliente'),
+    path('menu/', MenuView.as_view(), name='menu')
 ]
--- Apps/Cliente/views.py
+++ Apps/Cliente/views.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 class LoginView(TemplateView):
 	template_name = 'login.html'
 
-
 class CrearClienteView(TemplateView):
 	template_name = 'register.html'
+
+class MenuView(TemplateView):
+    template_name = 'Menu.html'
